27920616d93252e73c48c5da0cc5af9942c4c2ed
5chdn <5chdn@users.noreply.github.com>
2017-03-18 21:04:03 +0100

    Add javascript stub

---
+++ scripts/core.js
@@ -0,0 +1,7 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Title            8bit ethereum procedural transaction visualization   [8eth]
+/// Copyright        (c) 2017, Afri Schoedon                             [5chdn]
+/// License          GNU General Public License                            [GPL]
+/// License Version  3.0, 29 June 2007                                      [v3]
+////////////////////////////////////////////////////////////////////////////////
+
